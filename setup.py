5f7c1dac8238727cfc816dfda18a05bc68b7aab3
Etienne Charlier <bitbucket@procurasia.com>
2020-02-18 11:00:12 +0800

    Update setup.py with additional metadata and comments

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+# https://pythonhosted.org/an_example_pypi_project/setuptools.html
+
 setup(
  name='ecutils',
  version='0.0.1',
@@ -11,6 +13,13 @@ setup(
  license='LICENSE.txt',
  description='Set of utility functions used in several contexts',
  long_description=open('README.md').read(),
+ classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Utilities",
+],
  install_requires=[
    "bokeh",
    "IPython",
