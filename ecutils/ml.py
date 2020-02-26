c74f94b600f15cd4253f1d6a75bbd844eeb71d3d
Etienne Charlier <bitbucket@procurasia.com>
2020-02-26 17:23:28 +0800

    Add ml.py module

---
+++ ecutils/ml.py
@@ -0,0 +1,17 @@
+"""
+Utility Functions that can be used for Kaggle and other ML uses
+
+Includes all stable utility functions.
+"""
+
+import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import shutil
+
+from pathlib import Path
+from IPython.display import Image, display
+from pprint import pprint
+
