dcc8f6938eb73dd0a0d3cdc72490f12830bb475a
Etienne Charlier <github@procurasia.com>
2021-12-21 11:22:26 +0800

    Add nb_setup function

--- ecutils/ipython_utils.py
+++ ecutils/ipython_utils.py
@@ -4,9 +4,14 @@ This file includes a set of utility function to be used in Jupyter Notebook and 
 {License_info}
 """
 
+from IPython.core.getipython import get_ipython
 from IPython.display import display, Markdown, display_markdown
+from pathlib import Path
 
-__all__ = ['display_mds', 'display_dfs']
+import sys
+
+__all__ = ['display_mds', 'display_dfs', 'nb_setup']
+
 
 def display_mds(*strings):
     """
@@ -27,6 +32,47 @@ def display_dfs(*dfs):
     """
     for df in dfs:
         display(df)
+
+
+def nb_setup(autoreload=True, paths=None):
+    """Use in first cell of nb for setting up autoreload, paths, ...
+
+    By default, nb_setup() loads and set autoreload and adds a path to a directory named 'src'
+    at the same level as where the notebook directory is located.
+
+    By default, nb_setup assumes the following file structure:
+
+    project_directory
+          | --- notebooks
+          |        | --- current_nb.ipynb
+          |        | --- ...
+          |
+          |--- src
+          |     | --- scripts_to_import.py
+          |     | --- ...
+          |
+          |--- data
+          |     |
+          |     | ...
+    """
+#   Add paths. Default is 'src' if it exists
+    if paths is None:
+        p = Path('../src').resolve().absolute()
+        if p.is_dir():
+            paths = [str(p)]
+        else:
+            paths=[]
+    if paths:
+        for p in paths:
+            sys.path.insert(1, str(p))
+        print(f"Added following paths: {','.join(paths)}")
+
+#   Setup auto reload
+    if autoreload:
+        ipshell = get_ipython()
+        ipshell.run_line_magic('load_ext',  'autoreload')
+        ipshell.run_line_magic('autoreload', '2')
+        print('Set autoreload mode')
 
 
 if __name__ == '__main__':
