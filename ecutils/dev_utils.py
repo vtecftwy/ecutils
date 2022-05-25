17391d53a68d08efb52f09049bf10029bbb2be13
Etienne Charlier <github@procurasia.com>
2022-05-25 15:26:35 +0800

    Add new module for dev

---
+++ ecutils/dev_utils.py
@@ -0,0 +1,13 @@
+"""
+Utility Functions that can be used 
+
+Includes all stable utility functions that do not fit in other context.
+"""
+
+
+__all__ = []
+
+
+
+if __name__ == '__main__':
+    pass
