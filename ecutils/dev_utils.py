6c57e6b84659a12de003e5cd55de25319a4d6b07
Etienne Charlier <github@procurasia.com>
2022-05-26 14:32:05 +0800

    Add stack_trace decorators

--- ecutils/dev_utils.py
+++ ecutils/dev_utils.py
@@ -1,18 +1,20 @@
 """
-Utility Functions that can be used 
+Utility Functions that can be used in development phase
 
 Includes all stable utility functions that do not fit in other context.
 
 Thank you to socrateslee for his gist https://gist.github.com/socrateslee/4011475 for stack_trace
 """
+import re
 import sys
 import functools
 
-__all__ = ['stack_trace']
+__all__ = ['stack_trace', 'stack_trace_jupyter']
 
-class StackTrace(object):
-    def __init__(self, with_call=True, with_return=False,
-                       with_exception=False, max_depth=-1):
+
+class StackTrace():
+    """Capture and prints information on all stack frame executed"""
+    def __init__(self, with_call=True, with_return=False, with_exception=False, max_depth=-1):
         self._frame_dict = {}
         self._options = set()
         self._max_depth = max_depth
@@ -22,6 +24,9 @@ class StackTrace(object):
 
     def __call__(self, frame, event, arg):
         ret = []
+        co_name = frame.f_code.co_name
+        co_filename = frame.f_code.co_filename
+        co_lineno = frame.f_lineno
         if event == 'call':
             back_frame = frame.f_back
             if back_frame in self._frame_dict:
@@ -34,25 +39,74 @@ class StackTrace(object):
         if event in self._options\
           and (self._max_depth<0\
                or depth <= self._max_depth):
-            ret.append(frame.f_code.co_name)
+            ret.append(co_name)
             ret.append(f'[{event}]')
             if event == 'return':
                 ret.append(arg)
             elif event == 'exception':
                 ret.append(repr(arg[0]))
-            ret.append(f'in {frame.f_code.co_filename} line:{frame.f_lineno}')
+            ret.append(f'in {co_filename} line:{co_lineno}')
         if ret:
+            self.print_stack_info(co_filename, ret, depth)
+        return self
+
+    def print_stack_info(self, co_filename, ret, depth):
+        """This methods can be overloaded to customize what is printed out"""
+        text = '\t'.join([str(i) for i in ret])
+        print(f"{'  ' * depth}{text}")
+
+
+class StackTraceJupyter(StackTrace):
+    """Prints stack frame information in Jupyter notebook context (filters out jupyter overhead)"""
+
+
+
+    def print_stack_info(self, co_filename, ret, depth):
+        """Overload the base class to filter out those calls to Jupyter overhead functions"""
+
+        EXCL_LIBS = [
+            'encodings.*', 'ntpath.*', 'threading.*', 'weakref.*'
+        ]
+        EXCL_SITE_PACKAGES = [
+            'colorama', 'ipykernel', 'zmq'
+        ]
+
+        PATH_TO_LIBS_RE = r'^[a-zA-Z]:\\([^<>:\"/\\|?\*]*)\\envs\\([^<>:\"/\\|?\*]*)\\lib'
+        LIBS = f"{'|'.join(EXCL_LIBS)}"
+        SITE_PACKAGES = f"{'|'.join(EXCL_SITE_PACKAGES)}"
+        MODULE_FILTERS_RE = rf"{PATH_TO_LIBS_RE}\\(({LIBS})|(site-packages\\({SITE_PACKAGES}))\\.*)"
+
+        pat = re.compile(MODULE_FILTERS_RE)
+        match = pat.match(co_filename)
+        
+        if match is None:
+            """Only print stack frame info for those objects where there is no match"""
             text = '\t'.join([str(i) for i in ret])
             print(f"{'  ' * depth}{text}")
 
-        return self
 
 def stack_trace(**kw):
     """Function for stack_trace decorator"""
     def entangle(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            # st = StackTrace(**kw)
             st = StackTrace(**kw)
+            sys.settrace(st)
+            try:
+                return func(*args, **kwargs)
+            finally:
+                sys.settrace(None)
+        return wrapper
+    return entangle
+
+def stack_trace_jupyter(**kw):
+    """Function for stack_trace decorator"""
+    def entangle(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # st = StackTrace(**kw)
+            st = StackTraceJupyter(**kw)
             sys.settrace(st)
             try:
                 return func(*args, **kwargs)
@@ -64,4 +118,29 @@ def stack_trace(**kw):
 
 
 if __name__ == '__main__':
-    pass
+    from datetime import datetime
+    from dateutil import tz
+    from finutils.data_source_API_classes import MT4ServerTime
+
+    import pandas as pd
+
+
+    @stack_trace(with_return=True, with_exception=True, max_depth=3)
+    def test():
+        print('=== NY ==================================================================')
+        NY = tz.gettz('America/New_York')
+        print('=== MT4 ==================================================================')
+        MT4 = MT4ServerTime()
+
+    @stack_trace_jupyter(with_return=True, with_exception=True, max_depth=3)
+    def test_nb():
+        print('=== NY ==================================================================')
+        NY = tz.gettz('America/New_York')
+        print('=== MT4 ==================================================================')
+        MT4 = MT4ServerTime()
+
+
+    print(f"{'*'*100}")
+    test()
+    print(f"{'*'*100}")
+    test_nb()
